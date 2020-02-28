21a7af0d976a5dd67bdedf730827f06094a2d326
debauchery1st <trevor.j.martin@gmail.com>
2020-02-28 11:45:34 -0500

    jwt

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,23 @@
+const jwt = require("jsonwebtoken");
+const secrets = require("../server/secrets");
+
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const { authorization } = req.headers;
+  if (authorization) {
+    jwt.verify(authorization, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        res.status(401).json({ you: "shall not pass!" });
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "No credentials provided" });
+  }
 };
--- notes.md
+++ notes.md
@@ -1,8 +1,8 @@
 ## user
 
-| key      | type          |
-| :------- | :------------ |
-| username | VARCHAR(256)  |
-| password | VARCHAR(1024) |
+| key      | type         |
+| :------- | :----------- |
+| username | VARCHAR(255) |
+| password | VARCHAR(255) |
 
 A `user` has `username` and `password`. Both properties are required.
